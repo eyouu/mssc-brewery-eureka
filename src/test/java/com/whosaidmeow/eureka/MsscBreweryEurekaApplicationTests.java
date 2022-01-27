8ca40282aa828db2ef62d946284b969c6459b225
Vasyl <vazhovnir@gmail.com>
2022-01-27 17:53:42 +0200

    init

---
+++ src/test/java/com/whosaidmeow/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whosaidmeow.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
